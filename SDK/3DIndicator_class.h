0fb14475b105e73dc8e117d6b64c8c99c75449e7
Da3kL3o <107179124+Da3kL3o@users.noreply.github.com>
2022-06-09 17:48:43 +0900

    Add files via upload

---
+++ SDK/3DIndicator_class.h
@@ -0,0 +1,9 @@
+// WidgetBlueprintGeneratedClass 3DIndicator.3DIndicator_C
+// Size : 0x2F0 (Inherited : 0x2E8)
+struct U3DIndicator_C : UPD3DIndicatorUI {
+	struct FPointerToUberGraphFrame UberGraphFrame; // 0x2E8(0x8)
+
+	void Tick(struct FGeometry MyGeometry, float InDeltaTime); // Function 3DIndicator.3DIndicator_C.Tick // (BlueprintCosmetic|Event|Public|BlueprintEvent) // Param Size : 0x3C, 0x7FF653FAFDF0
+	void ExecuteUbergraph_3DIndicator(int32_t EntryPoint); // Function 3DIndicator.3DIndicator_C.ExecuteUbergraph_3DIndicator // (Final|UbergraphFunction|HasDefaults) // Param Size : 0x4, 0x7FF653FAFDF0
+};
+
